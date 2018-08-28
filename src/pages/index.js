f0021dba552c424a3651cf3befde89b354ff1f85
Falkhan <falkhan.mage@gmail.com>
2018-08-28 13:29:01 +0200

    more info

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,8 @@ const IndexPage = () => (
   <div>
     <h1>Hello there</h1>
     <p>My name is Piotr and I like programming.</p>
-    <p>I have never used Gatsby before, but it looks like something worth learning about!</p>
+    <p>I am currently studying Management and Politics. I was always interested in coding and decided to pursue a career in IT after graduation.</p>
+    <p>I learnt a lot using free sites such as freeCodeCamp (so thank you for that!)</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
